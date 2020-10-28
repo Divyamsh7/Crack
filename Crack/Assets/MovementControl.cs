fc40503ae9c082e71080fa18db35ceac8a87f6f7
Kokyne7 <73416753+Kokyne7@users.noreply.github.com>
2020-10-29 03:13:00 +0530

    Movement added

---
+++ Crack/Assets/MovementControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MovementControl : MonoBehaviour
+{
+    float dirX;
+
+    public float moveSpeed = 1f, jumpForce = 700f;
+
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        dirX = CrossPlatformInputManager.VirtualAxisReference("Horizontal").GetValue;
+        Debug.Log("dir value "+dirX);
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            DoJump();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (dirX * moveSpeed, rb.velocity.y);
+    }
+
+    public void DoJump()
+    {
+        if (rb.velocity.y == 0)
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+    }
+}
